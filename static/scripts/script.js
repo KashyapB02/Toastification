5163138bd36da8ddb843f263e4468e4764359ec8
Bobby Kumar <kashyapashish0002@gmail.com>
2022-09-18 18:36:47 +0530

    Added toast position option in form

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -6,57 +6,49 @@ const sampleToastBtn = document.getElementById("sampleToastBtn");
 toastGenerateForm.onsubmit = (e) => {
     e.preventDefault();
 
+    const toastPosition = toastGenerateForm.toast_position;
     const toastType = toastGenerateForm.toast_type;
     const toastTitle = toastGenerateForm.toast_title;
     const toastDescription = toastGenerateForm.toast_description;
 
+    const toast = {
+        position: toastPosition.value,
+        title: toastTitle.value,
+        message: toastDescription.value
+    }
+
     switch(toastType.value) {
-        case "info": {
-            infoToast(toastTitle.value, toastDescription.value);
-            break;
-        }
-
-        case "warning": {
-            warningToast(toastTitle.value, toastDescription.value);
-            break;
-        }
-
-        case "error": {
-            errorToast(toastTitle.value, toastDescription.value);
-            break;
-        }
-
-        case "Success": {
-            successToast(toastTitle.value, toastDescription.value);
-            break;
-        }
+        case "info": { infoToast(toast); break; }
+        case "warning": { warningToast(toast); break; }
+        case "error": { errorToast(toast); break; }
+        case "Success": { successToast(toast); break; }
     }
 
     toastGenerateForm.reset(); 
 }
 
 sampleToastBtn.onclick = () => {
-    let random = Math.floor(Math.random() * 4);
+    let randomToast = Math.floor(Math.random() * 4);
+    let randomPosition = Math.floor(Math.random() * 6);
 
-    switch(random) {
-        case 0: {
-            infoToast("Lorem Ipsum", "Lorem ipsum dolor sit, amet consectetur adipisicing elit.");
-            break;
-        }
+    const positions = ["topRight", "topCenter", "topLeft", "bottomRight", "bottomCenter", "bottomLeft"];
+    const descriptions = [
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        "Cum dicta quisquam et labore temporibus recusandae, accusantium delectus vel!",
+        "Enim iusto, nesciunt suscipit quasi illo officiis quisquam atque velit magnam doloremque ea quos.",
+        "voluptatibus fugit nam accusantium provident architecto rerum corrupti?"
+    ]
 
-        case 1: {
-            warningToast("Lorem Ipsum", "Cum dicta quisquam et labore temporibus recusandae, accusantium delectus vel!");
-            break;
-        }
+    const toast = {
+        position: positions[randomPosition],
+        title: "Lorem Ipsum",
+        message: descriptions[randomToast]
+    }
 
-        case 2: {
-            errorToast("Lorem Ipsum", "Enim iusto, nesciunt suscipit quasi illo officiis quisquam atque velit magnam doloremque ea quos.");
-            break;
-        }
-
-        case 3: {
-            successToast("Lorem Ipsum", "voluptatibus fugit nam accusantium provident architecto rerum corrupti?");
-            break;
-        }
+    switch(randomToast) {
+        case 0: { infoToast(toast); break; }
+        case 1: { warningToast(toast); break; }
+        case 2: { errorToast(toast); break; }
+        case 3: { successToast(toast); break; }
     }
 }
--- static/toast/toast.js
+++ static/toast/toast.js
@@ -1,54 +1,89 @@
-let cssId = "toastCss";
+let toastId = "toastCss";
+let toastAnimationId = "toastAnimationCss";
 
-if (!document.getElementById(cssId)) {
-    let origin = window.location.origin;
+[
+    { id: toastId, stylesheet: "toast" },
+    { id: toastAnimationId, stylesheet: "toast-animation" }
+].forEach((item) => {
+    if (!document.getElementById(item.id)) {
+        let origin = window.location.origin;
+        let head = document.getElementsByTagName("head")[0];
 
-    const head = document.getElementsByTagName("head")[0];
-    const link = document.createElement("link");
-    link.setAttribute("id", cssId);
-    link.setAttribute("rel", "stylesheet");
-    link.setAttribute("type", "text/css");
-    link.setAttribute("href", `${origin}/static/toast/toast.css`);
-    link.setAttribute("media", "screen,print");
+        let link = document.createElement("link");
+        link.setAttribute("id", item.id);
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("type", "text/css");
 
-    head.appendChild(link);
-}
+        // TODO: Change the value of 'href' attribute according to
+        //       the placement of '/toast' folder in your file structure.
+        link.setAttribute("href", `${origin}/static/toast/${item.stylesheet}.css`);
+        link.setAttribute("media", "screen,print");
+        head.appendChild(link);
+    }
+})
 
-function toast (title, message, svg, toastType) {
+function prepareToast (props) {
     let ativeTimeout, start, remaining;
 
-    const toast = `<div class="toast" id="toast"></div>`
+    let toast = `<div class="toast ${props.position}" id="toast"></div>`
+    let toastDiv = null;
+
     if (!document.getElementById("toast"))
         document.body.insertAdjacentHTML("afterbegin", toast);
+    else {
+        const toastDivs = document.querySelectorAll("#toast");
 
-    const toastDiv = document.getElementById("toast")
-    if (toastDiv.childElementCount) {
-        toastDiv.removeChild(toastDiv.lastChild)
+        for (let div of toastDivs) {
+            if (div.classList.contains(props.position)) {
+                toastDiv = div;
+                break;
+            }
+        }
 
-        if (ativeTimeout)
-            clearTimeout(ativeTimeout);
+        if (!toastDiv) {
+            document.body.insertAdjacentHTML("afterbegin", toast);
+            const toastDivList = document.querySelectorAll("#toast")
+            toastDiv = [...toastDivList][0];
+        }
     }
 
-    const toastContainer = `
-        <div class="toastContainer toastActive" id="toastContainer">
+    if (!toastDiv)
+        toastDiv = document.getElementById("toast");
+
+    let activeAnimationClass = null;
+    let closeAnimationClass = null;
+
+    if ((props.position === "topRight") || (props.position === "bottomRight")) {
+        activeAnimationClass = "rightActiveAnimation";
+        closeAnimationClass = "rightCloseAnimation";
+    } else if ((props.position === "topLeft") || (props.position === "bottomLeft")) {
+        activeAnimationClass = "leftActiveAnimation";
+        closeAnimationClass = "leftCloseAnimation";
+    } else {
+        activeAnimationClass = `${props.position}ActiveAnimation`;
+        closeAnimationClass = `${props.position}CloseAnimation`;
+    }
+
+    let toastContainer = `
+        <div class="toastContainer ${activeAnimationClass}" id="toastContainer">
             <div class="toastContent">
                 <div class="toastTitleContainer">
-                    <span class="toastTypeIcon ${toastType}">${svg}</span>
-                    <p class="toastTitle">${title}</p>
+                    <span class="toastTypeIcon ${props.type}">${props.svg}</span>
+                    <p class="toastTitle">${props.title}</p>
                 </div>
-                <p class="toastMsg">${message}</p>
+                <p class="toastMsg">${props.msg}</p>
             </div>
             <span class="toastCloseBtn" id="toastCloseBtn" title="Close"><p></p></span>
         </div>
     `
 
     toastDiv.insertAdjacentHTML("beforeend", toastContainer);
-    const toastContainerDivList = document.querySelectorAll("#toastContainer");
+    const toastContainerDivList = toastDiv.querySelectorAll("#toastContainer");
     const toastContainerDiv = [...toastContainerDivList].pop();
 
     function handleToast () {
-        toastContainerDiv.classList.remove("toastActive");
-        toastContainerDiv.classList.add("toastClose");
+        toastContainerDiv.classList.remove(activeAnimationClass);
+        toastContainerDiv.classList.add(closeAnimationClass);
 
         setTimeout(function () {
             (toastContainerDiv.parentNode).removeChild(toastContainerDiv);
@@ -60,7 +95,7 @@ function toast (title, message, svg, toastType) {
         start = Date.now();
     }
 
-    const toastCloseBtnList = document.querySelectorAll("#toastCloseBtn");
+    const toastCloseBtnList = toastDiv.querySelectorAll("#toastCloseBtn");
     const toastCloseBtn = [...toastCloseBtnList].pop();
     toastCloseBtn.onclick = function () { handleToast(); }
 
@@ -79,42 +114,73 @@ function toast (title, message, svg, toastType) {
     }
 }
 
-export const successToast = (title, message) => {
+export const successToast = (toast) => {
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/>
         </svg>
     `
 
-    toast(title, message, svg, "successToast");
+    const props = {
+        position: toast.position,
+        title: toast.title,
+        msg: toast.message,
+        svg: svg,
+        type: "successToast"
+    }
+
+    prepareToast(props);
 }
 
-export const errorToast = (title, message) => {
+export const errorToast = (toast) => {
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
         </svg>
     `
 
-    toast(title, message, svg, "errorToast");
+    const props = {
+        position: toast.position,
+        title: toast.title,
+        msg: toast.message,
+        svg: svg,
+        type: "errorToast"
+    }
+
+    prepareToast(props);
 }
 
-export const warningToast = (title, message) => {
+export const warningToast = (toast) => {
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zm32 224c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/>
         </svg>
     `
 
-    toast(title, message, svg, "warningToast");
+    const props = {
+        position: toast.position,
+        title: toast.title,
+        msg: toast.message,
+        svg: svg,
+        type: "warningToast"
+    }
+
+    prepareToast(props);
 }
 
-export const infoToast = (title, message) => {
+export const infoToast = (toast) => {
     const svg = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-144c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/>
         </svg>
     `
+    const props = {
+        position: toast.position,
+        title: toast.title,
+        msg: toast.message,
+        svg: svg,
+        type: "infoToast"
+    }
 
-    toast(title, message, svg, "infoToast");
+    prepareToast(props);
 }
